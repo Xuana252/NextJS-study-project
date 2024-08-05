f07f1d4e315707a51c61ad7e49a504a0b6958d8b
Xuana252 <notfuny4927@gmail.com>
2024-08-05 16:04:49 +0700

    fix useSearchParams

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,12 +1,12 @@
 "use client"
 import React, {useState,useEffect} from 'react'
-import { useRouter,useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import Form from '@components/Form'
 
 
 const EditPrompt = () => {
   const router = useRouter()
-  const [searchParams]= useSearchParams()
+  const searchParams= useSearchParams()
   const [submitting,setSubmitting] = useState(false)
   const [post,setPost] = useState({
     prompt: '',
